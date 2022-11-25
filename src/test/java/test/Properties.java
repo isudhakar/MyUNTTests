1561fa5b5b42d9c1969965ac7ae976c51c6090eb
isudhakar <tallurisudhakar@hotmail.com>
2022-11-25 04:49:32 -0600

    Update properties

---
+++ src/test/java/test/Properties.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Properties {
+	public static String username = "your_username";
+	public static String password = "**********";
+	public static String rollno = "your_rollNo";
+	public static String enrollClass = "Subjectid";
+}
